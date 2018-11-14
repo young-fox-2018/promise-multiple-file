7af70f61b943e539f85d8d35d338dfacdc5d2905
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-14 11:22:30 +0700

    init commit

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function readFilePromise() {
-  // psst, the promise should be around here...
+function readFilePromise(file) {
+  return new Promise((resolve, reject) => {
+    //TODO:
+
+    fs.readFile(`${file}`, 'utf8', (err, data) => {
+      err ? reject(err) : resolve(data)
+    })
+  })
 }
 
+function matchData(parentfile, childrenfile) {
+  let dataParent = JSON.parse(parentfile)
+  let dataChildren = JSON.parse(childrenfile)
+
+  for (let i = 0; i < dataParent.length; i++) {
+    dataParent[i].children = []
+    for (let j = 0; j < dataChildren.length; j++) {
+      if (dataChildren[j].family == dataParent[i].last_name) {
+        dataParent[i].children.push(dataChildren[j].full_name)
+      }
+    }
+  }
+
+  console.log(dataParent);
+  
+}
+
+
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
-  // your code here... (p.s. readFilePromise function(s) should be around here..)
+  let parent_data = undefined
+  let children_data = undefined
+  readFilePromise(parentFileName).then((data) => {
+    parent_data = data
+    return readFilePromise(childrenFileName)
+  }).then((data1) => {
+    children_data = data1
+    matchData(parent_data, children_data)
+  })
+  
+
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');
 console.log("Notification : Data sedang diproses !");
 
 // for Release 2
-matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
-matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
+// matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
+// matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
