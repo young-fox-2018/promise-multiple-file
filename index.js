e873a1f1180f56bf634c82d55f9b632eff0ce78f
ranggakusuma <ranggavskusuma@gmail.com>
2018-11-14 11:58:50 +0700

    multiple file done

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function readFilePromise() {
-  // psst, the promise should be around here...
+function readFilePromise(file) {
+  return new Promise ((resolve, reject) => {
+    fs.readFile(file, 'utf8', function(err, data) {
+      sleep.sleep(1)
+      if (err) {
+        reject(err)
+      } else {
+        resolve(JSON.parse(data))
+      }
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
-  // your code here... (p.s. readFilePromise function(s) should be around here..)
+  let dataParent = []
+  readFilePromise(parentFileName)
+    .then((dataParents) => {
+      // console.log(dataParent)
+      dataParent = dataParents
+      return readFilePromise(childrenFileName)
+    })
+    .then((dataChild) => {
+
+      dataParent.forEach(element => {
+        element.childrens = dataChild.filter((elementChild) => element.last_name === elementChild.family)
+        .map((datum) => datum.full_name)
+      });
+
+      console.log(dataParent)
+
+    })
+    .catch((err) => {
+      console.log({
+        message: 'Terjadi error pada proses pembacaan data',
+        err:err
+      })
+    })
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');
