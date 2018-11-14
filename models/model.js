0686243bbcbc10bbbab0b984e6609d8f3f5967c4
vinutama <muhammadkhevin@gmail.com>
2018-11-14 12:58:52 +0700

    add sleep

--- models/model.js
+++ models/model.js
@@ -13,7 +13,17 @@ class Model {
         })
 
     }
+    static sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds) {
+                break;
+            }
+        }
+    }
     static matchParentsWithChildren() {
+        console.log('Notifikasi: Data sedang Di proses!!')
+        Model.sleep(5000)
         return Promise.all([Model.readFilePromise('./parents.json'), Model.readFilePromise('./childrens.json')])
     }
 
